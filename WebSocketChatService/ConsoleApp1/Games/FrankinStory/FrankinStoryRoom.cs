5be7e38c195d04306b2de7bcf0da3a2dcccf5ba2
uastephen <uastephen@gmail.com>
2020-02-22 16:41:30 -0500

    a;lskdfj;asldfjk

--- WebSocketChatService/ConsoleApp1/Games/FrankinStory/FrankinStoryRoom.cs
+++ WebSocketChatService/ConsoleApp1/Games/FrankinStory/FrankinStoryRoom.cs
@@ -1,5 +1,7 @@
 ﻿using ChatService.ChatSupport;
+using ChatService.types;
 using CSharpPacheCore.Streamers;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,16 +10,52 @@ namespace ChatService.Games.FrankinStory
 {
     public class FrankinStoryRoom : IGameRoom
     {
-        public string name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public string topic => throw new NotImplementedException();
-
-        public string instance => throw new NotImplementedException();
-
+        string Instance = Guid.NewGuid().ToString().Replace("-", "");
+        public string instance { get { return Instance; } }
+        public string name { get; set; }
+        public string topic { get { return "FrankinStory"; } }
+        Dictionary<String, CPacheStream> cpacheStreams = new Dictionary<string, CPacheStream>();
+        public Dictionary<String, Player> players = new Dictionary<string, Player>();
+        object locker = new object();
+        List<ChatLine> chatContent = new List<ChatLine>();
+        List<CPacheStream> disposedRunningList = new List<CPacheStream>();
         public void addLine(ChatLine chatLine, CPacheStream cPacheStream)
         {
-            throw new NotImplementedException();
+            if (cpacheStreams.ContainsKey(cPacheStream.StreamId))
+            {
+                cpacheStreams.Remove(cPacheStream.StreamId);
+                cpacheStreams.Add(chatLine.userName, cPacheStream);
+                if (players.Count == 0)
+                {
+                    players.Add(chatLine.userName, new Player() { myTurn = true, Score = 0, Username = chatLine.userName });
+                }
+                else
+                {
+                    players.Add(chatLine.userName, new Player() { myTurn = false, Score = 0, Username = chatLine.userName });
+                }
+            }
+
+
+            lock (locker)
+            {
+                //add it to the legder 
+                chatContent.Add(chatLine);
+
+                foreach (KeyValuePair<String, CPacheStream> client in cpacheStreams)
+                {
+                    var ret = JsonConvert.SerializeObject(chatLine);
+                    try
+                    {
+                        client.Value.Broadcast(ret);
+                    }
+                    catch (Exception)
+                    {
+                        disposedRunningList.Add(client.Value);
+                    }
+                }
+            }
         }
+
 
         public string handleClientAppMessage(ChatLine chat)
         {
@@ -26,7 +64,17 @@ namespace ChatService.Games.FrankinStory
 
         public void subscribeToChat(CPacheStream cPacheStream)
         {
-            throw new NotImplementedException();
+            lock (locker)
+            {
+                //add the client to new messages 
+                cpacheStreams.Add(cPacheStream.StreamId, cPacheStream);
+                //send all the old messages 
+                foreach (ChatLine chatLine in chatContent)
+                {
+                    var ret = JsonConvert.SerializeObject(chatLine);
+                    cPacheStream.Broadcast(ret);
+                }
+            }
         }
     }
 }
--- WebSocketChatService/ConsoleApp1/bin/Debug/netcoreapp3.1/html/js/ChatService.js
+++ WebSocketChatService/ConsoleApp1/bin/Debug/netcoreapp3.1/html/js/ChatService.js
@@ -8,6 +8,9 @@ ChatServiceApp.config(function ($routeProvider) {
         .when('/Home', {
             templateUrl: 'views/home.html'
         })
+        .when('/FrankinStory', {
+            templateUrl: 'views/FrankinStory.html'
+        })
         .when('/CaH', {
             templateUrl: 'views/CaH.html'
         });
@@ -208,6 +211,202 @@ ChatServiceApp.controller('CaHController', function ($scope, $routeParams, $loca
     $scope.init = function () {
         $scope.Message = "Loading";
         socket = new WebSocket("ws://" + location.host + "/api/CaHService?roomid=" + $rootScope.RoomId);
+        socket.onopen = (function (event) {
+            $scope.Validated = true;
+            $scope.Message = "Access Approved";
+            $scope.$apply();
+        });
+        socket.addEventListener('message', function (event) {
+            $scope.update(event);
+            $scope.$apply();
+        });
+
+        $http.get("/api/Colors?roomid=" + $rootScope.RoomId).then(function (pasdfj) {
+            $scope.Colors = pasdfj.data;
+        }, function (p) {
+            console.log('Failed');
+        });
+
+        socket.onclose = function (event) {
+            $scope.Validated = false;
+            $scope.Message = "Connection Closed";
+            $scope.$apply();
+        };
+    };
+    $scope.init();
+});
+
+
+ChatServiceApp.controller('FrankinStoryController', function ($scope, $routeParams, $location, $rootScope, $http) {
+
+    var socket = [];
+    $scope.Messages = [];
+    $scope.Message = "";
+    $scope.Validated = false;
+    $scope.lightson = false;
+    $scope.myturn = false;
+    $scope.LoggedIn = false;
+    $scope.Colors = [];
+    $scope.selectedColor = "";
+    $scope.UsernameDisabled = false;
+    $scope.myWhiteCards = [];
+    $scope.PossWinnerWhiteCards = [];
+    $scope.Username = "";
+    $scope.showMyWhite = false;
+    $scope.showPossWhite = false;
+    $scope.players = [];
+    $scope.blackCard = "";
+    $scope.gamecontext = [];
+    $scope.currentplayer = "";
+
+    $scope.updatefromsidecar = function (data) {
+        var switchVal = data.substring(7, 10);
+        switch (switchVal) {
+            //Start the game
+            case "STG":
+                console.log("Start the game");
+                var context = JSON.parse(data.replace("SideCarSTG", ""));
+                $scope.PossWinnerWhiteCards = [];
+                $scope.players = context.players;
+                $scope.blackCard = context.BlackCard;
+                $scope.gamecontext = context;
+
+                $scope.gamecontext.players.forEach(function (p) {
+                    if (p.myTurn) {
+                        $scope.currentplayer = p.Username;
+                    }
+                });
+                context.players.forEach(function (p) {
+                    if (p.Username == $scope.Username) {
+                        if (p.myTurn == true) {
+                            $scope.myturn = true;
+                            $scope.Message = "Wait For Everyone To Play Their White Cards";
+                            $scope.showMyWhite = false;
+                        } else {
+                            $scope.myturn = false;
+                        }
+                    }
+                    if ($scope.myturn == false) {
+                        $scope.Message = "Select A White Card To Play";
+                        $scope.showMyWhite = true;
+                        $scope.showPossWhite = false;
+                        $scope.PossWinnerWhiteCards = [];
+                    }
+                });
+
+                break;
+            // Select the Winner
+            case "STW":
+                console.log("Select the Winner");
+                break;
+            // Pick Your White Card It's time to select a winner 
+            case "PWC":
+                console.log("Possible White Card");
+                var posscardcontext = JSON.parse(data.replace("SideCarPWC", ""));
+                $scope.PossWinnerWhiteCards.push(posscardcontext);
+                if ($scope.myturn) {
+                    $scope.Message = "Select A White Card To Win";
+                } else {
+
+
+                    $scope.Message = "Waiting For " + $scope.currentplayer + " To Pick A Card";
+                }
+                $scope.showPossWhite = true;
+                break;
+            // New White Card 
+            case "HWC":
+                var cardcontext = JSON.parse(data.replace("SideCarHWC", ""));
+                $scope.myWhiteCards.push(cardcontext);
+                break;
+            // Wait For White Cards. 
+            case "WFC":
+                console.log("Wait For White Cards.");
+                break;
+            // Announce Winning White Card 
+            case "WWC":
+                var winningCard = JSON.parse(data.replace("SideCarWWC", ""));
+                winningCard.content = "Winner Is: " + winningCard.content;
+                $scope.PossWinnerWhiteCards = [];
+                $scope.PossWinnerWhiteCards.push(winningCard);
+                break;
+
+
+        }
+    };
+    $scope.update = function (eventdata) {
+        if (eventdata.data.substring(0, 7) == "SideCar") {
+            $scope.updatefromsidecar(eventdata.data);
+            return;
+        }
+        var obj = JSON.parse(eventdata.data);
+        if ($scope.Username == obj.userName) {
+            obj.class = "sent msg";
+        } else {
+            obj.class = "rcvd msg";
+        }
+        $scope.Messages.push(obj);
+    };
+    $scope.Login = function () {
+        $scope.LoggedIn = true;
+
+        $scope.CurrentText = "User Logged In";
+        $scope.SendMemo();
+
+    };
+
+    $scope.sendWhiteCard = function (cardValue) {
+        $scope.myWhiteCards.splice($scope.myWhiteCards.indexOf(cardValue), 1);
+        $scope.Message = "Wait for the other players";
+        $scope.showMyWhite = false;
+        var message = {
+            "content": "SideCarWCD" + cardValue.content,
+            "userName": $scope.Username,
+            "dateTime": "11/16/1991",
+            "userColor": "red"
+        };
+
+        socket.send(JSON.stringify(message));
+    };
+    $scope.selectWinningWhiteCard = function (cardValue) {
+
+        if (!$scope.myturn) {
+            return;
+        }
+
+
+        var message = {
+            "content": "SideCarWCS" + cardValue.content,
+            "userName": $scope.Username,
+            "dateTime": "11/16/1991",
+            "userColor": "red"
+        };
+
+        socket.send(JSON.stringify(message));
+    };
+
+
+
+
+    $scope.SendMemo = function () {
+        if (!$scope.Username || $scope.Username == "") {
+            console.log('give a username first');
+        }
+        if ($scope.UsernameDisabled == false) {
+            $scope.UsernameDisabled = true;
+        }
+        var message = {
+            "content": $scope.CurrentText,
+            "userName": $scope.Username,
+            "dateTime": "11/16/1991",
+            "userColor": "red"
+        };
+
+        socket.send(JSON.stringify(message));
+        $scope.CurrentText = "";
+    };
+    $scope.init = function () {
+        $scope.Message = "Loading";
+        socket = new WebSocket("ws://" + location.host + "/api/FrankinStoryService?roomid=" + $rootScope.RoomId);
         socket.onopen = (function (event) {
             $scope.Validated = true;
             $scope.Message = "Access Approved";
